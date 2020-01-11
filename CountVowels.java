d89a1cd67f6904112c44f73576e6d90ad82f70d0
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2020-01-11 23:18:03 +1100

    Add files via upload

---
+++ CountVowels.java
@@ -0,0 +1,26 @@
+/*public class CountVowels {
+    public static void main(String[] args) {
+        String s = "astronaut";
+        int count = countVowels(s);
+        System.out.println("Number of count: " + count);
+    }
+    public static int countVowels(String s) {
+        if (s == null) {
+            return -1}
+        }
+
+
+    }*/
+
+public class CountVowels {
+    public static int countVowels(String s) {
+        if (s == null) {
+            return -1;
+        }
+        int sum = 0
+        char[] vowels = new char[]{'a','e','i','o','u'};
+        for (int i = 0; i < s.length; s++) {
+            for (int j = 0; j < s.length
+        }
+    }
+}
