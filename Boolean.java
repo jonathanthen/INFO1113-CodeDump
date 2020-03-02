e6fd4c7fc13e59aa130d42d132d53e3cfde85cfb
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2020-03-02 11:37:50 +1100

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,11 @@
+public class Boolean {
+    public static void main(String[] args){
+        boolean t = true;
+        boolean f = false;
+
+        boolean exampleAnd = t && t; // True
+        boolean exampleOr = f || t; // True
+        boolean exampleNot = !t; // False
+
+    }
+}
