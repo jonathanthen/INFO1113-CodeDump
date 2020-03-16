0ac880efacff060fa97a8eb6af9fa83fff60e140
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2020-03-17 10:38:46 +1100

    Add files via upload

---
+++ Cupcake.java
@@ -0,0 +1,10 @@
+public class Cupcake {
+    boolean delicious;
+    String name;
+
+    public Cupcake(boolean isDelicious, String n) {
+        delicious = isDelicious;
+        name = n;
+    }
+
+}
