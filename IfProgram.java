d567fd20fa1d8ad4b108bad47c152672a66bde3f
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2020-04-06 11:54:09 +1000

    Add files via upload

---
+++ IfProgram.java
@@ -0,0 +1,14 @@
+public class IfProgram {
+    public static void main(String[] args){
+        boolean t = true;
+        boolean f = false;
+
+        if(t) {
+            System.out.println("This will be executed");
+        }
+
+        if(f) {
+            System.out.println("This will not be executed");
+        }
+    }
+}
