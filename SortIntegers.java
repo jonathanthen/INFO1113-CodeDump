2de936f2e580b4b2b641e2e15b75646c4db41986
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2020-05-09 16:05:40 +1000

    Add files via upload

---
+++ SortIntegers.java
@@ -0,0 +1,16 @@
+public class SortIntegers {
+	
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 5;
+		int z = 3;
+		
+		if (x>y && x>z) {
+			System.out.println(x);
+		} else if (y>x && y >z) {
+			System.out.println(y);
+		} else {
+			System.out.println(z);
+		}
+	}
+}
