5f0e79dca0912a86e71e059fa02e8c87be46925d
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2020-05-16 12:06:36 +1000

    Add files via upload

---
+++ Tumbler.java
@@ -0,0 +1,11 @@
+public class Tumbler {
+  
+    private String colour;
+    
+    
+    public Tumbler(String colour) {
+      this.colour = colour;
+    }
+    
+    public String colour() { return colour; }
+  }
