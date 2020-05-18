f3e0e3c36b17b6756f8b0a03dc100ca499954a6d
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2020-05-18 13:47:33 +1000

    Add files via upload

---
+++ TwoDeeArray.java
@@ -0,0 +1,12 @@
+public class TwoDeeArray {
+    public static void main(String[] args) {
+        int[][] arr = new int [5][5];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j]);
+            }
+
+            System.out.println();
+        }
+    }
+}
