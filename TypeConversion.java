8e6fa0b7a80ca5e80f3c5488b176f29a55c575f4
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2020-05-19 22:21:29 +1000

    Add files via upload

---
+++ TypeConversion.java
@@ -0,0 +1,10 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        int i = 1;
+        double f = 1.0;
+
+        System.out.println(i/2);
+        System.out.println(f/2);
+        System.out.println(f/i);
+    }
+}
